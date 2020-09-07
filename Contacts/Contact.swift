4cbfe7a1cdbc661b3b6482965c0d4b591742ba12
Akhil Jain <Akhil.Jain@go-mmt.com>
2020-09-07 10:50:49 +0530

    first commit

---
+++ Contacts/Contact.swift
@@ -0,0 +1,9 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Akhil Jain on 04/09/20.
+//  Copyright © 2020 Akhil Jain. All rights reserved.
+//
+
+import Foundation
---
+++ Contacts/ContactInfo.swift
@@ -0,0 +1,9 @@
+//
+//  ContactInfo.swift
+//  Contacts
+//
+//  Created by Akhil Jain on 03/09/20.
+//  Copyright © 2020 Akhil Jain. All rights reserved.
+//
+
+import Foundation
